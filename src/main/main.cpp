a4141e50d19866c5ee8b419c53cd3617f9c5b671
Emmanuel Jay <emmanuel@classnco.com>
2015-09-19 12:11:35 +0200

    Addition of a Logger for the project

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,16 +1,32 @@
-#include <iostream>
-
 #include "CMakeParams.h"
 
+#include <glog/logging.h>
 
 int main(int argc, char* argv[])
 {
-  std::cout 
+  // Initialize Google's logging library.
+  FLAGS_stderrthreshold = 0;
+  FLAGS_log_dir = "../logs";
+  google::InitGoogleLogging(argv[0]);
+
+  LOG(INFO)
     << "Using CMake Build in " << CMAKE_BUILD_TYPE
     << " mode -- Rasta IRP Version : "
     << RASTA_IRP_VERSION_MAJOR << "."
     << RASTA_IRP_VERSION_MINOR << "."
-    << RASTA_IRP_VERSION_PATCH << std::endl;
-
+    << RASTA_IRP_VERSION_PATCH;
+  LOG(ERROR)
+    << "Using CMake Build in " << CMAKE_BUILD_TYPE
+    << " mode -- Rasta IRP Version : "
+    << RASTA_IRP_VERSION_MAJOR << "."
+    << RASTA_IRP_VERSION_MINOR << "."
+    << RASTA_IRP_VERSION_PATCH;
+  LOG(WARNING)
+    << "Using CMake Build in " << CMAKE_BUILD_TYPE
+    << " mode -- Rasta IRP Version : "
+    << RASTA_IRP_VERSION_MAJOR << "."
+    << RASTA_IRP_VERSION_MINOR << "."
+    << RASTA_IRP_VERSION_PATCH;
+    
   return 0;
 }
